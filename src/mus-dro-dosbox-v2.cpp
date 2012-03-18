a5c06a2f43f7506b087c2d737bdb0c49cf08f7d8
Adam Nielsen <malvineous@shikadi.net>
2012-03-18 16:19:29 +1000

    Update DROv2 handler to new architecture

--- src/mus-dro-dosbox-v2.cpp
+++ src/mus-dro-dosbox-v2.cpp
@@ -2,7 +2,7 @@
  * @file   mus-dro-dosbox-v2.cpp
  * @brief  Support for the second version of the DOSBox Raw OPL .DRO format.
  *
- * Copyright (C) 2010-2011 Adam Nielsen <malvineous@shikadi.net>
+ * Copyright (C) 2010-2012 Adam Nielsen <malvineous@shikadi.net>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,11 +18,257 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <iostream>
 #include <camoto/iostream_helpers.hpp>
+#include <camoto/stream_string.hpp>
+#include "decode-opl.hpp"
+#include "encode-opl.hpp"
+#include "metadata-malv.hpp"
 #include "mus-dro-dosbox-v2.hpp"
 
 using namespace camoto;
 using namespace camoto::gamemusic;
+
+/// Length of each .dro tick in microseconds
+#define DRO_CLOCK  1000
+
+/// Command byte we will use to write a short delay
+#define DRO2_CMD_SHORTDELAY 0xFF
+
+/// Command byte we will use to write a long delay
+#define DRO2_CMD_LONGDELAY 0xFE
+
+/// Decode data in a .dro file to provide register/value pairs.
+class OPLReaderCallback_DRO_v2: virtual public OPLReaderCallback
+{
+	public:
+		OPLReaderCallback_DRO_v2(stream::input_sptr input)
+			throw (stream::error)
+			: input(input),
+			  first(true)
+		{
+			this->input->seekg(12, stream::start);
+			this->input >> u32le(this->lenData);
+			this->input->seekg(6, stream::cur);
+			uint8_t compression;
+			this->input
+				>> u8(compression)
+				>> u8(this->codeShortDelay)
+				>> u8(this->codeLongDelay)
+				>> u8(this->codemapLength)
+			;
+			if (compression != 0) throw stream::error("Compressed DRO files are not implemented (didn't even know they existed)");
+			if (this->codemapLength > 127) throw stream::error("DRO code map too large");
+			memset(this->codemap, 0xFF, sizeof(this->codemap));
+			this->input->read(this->codemap, this->codemapLength);
+			// Seek pointer is now at start of OPL data
+		}
+
+		virtual bool readNextPair(OPLEvent *oplEvent)
+			throw (stream::error)
+		{
+			if (this->lenData == 0) return false;
+
+			oplEvent->delay = 0;
+
+			if (this->first) {
+				// First call, set tempo
+				this->first = false;
+				oplEvent->tempo = DRO_CLOCK;
+				oplEvent->reg = 0;
+				oplEvent->val = 0;
+				oplEvent->chipIndex = 0;
+				return true; // empty event w/ initial tempo
+			}
+
+			try {
+				uint8_t code, arg;
+nextCode:
+				this->input >> u8(code) >> u8(arg);
+				this->lenData--;
+				if (code == this->codeShortDelay) {
+					oplEvent->delay += arg + 1;
+					goto nextCode;
+				} else if (code == this->codeLongDelay) {
+					oplEvent->delay += (arg + 1) << 8;
+					goto nextCode;
+				}
+
+				// High bit indicates which chip to use
+				oplEvent->chipIndex = code >> 7;
+
+				if ((code & 0x7F) >= this->codemapLength) {
+					std::cerr << "WARNING: DRO file is using codes past the end of the code "
+						"map!" << std::endl;
+					// But continue as we will just use register 0xFF in these cases.
+				}
+				oplEvent->reg = this->codemap[code & 0x7F];
+				oplEvent->val = arg;
+			} catch (const stream::incomplete_read&) {
+				return false;
+			}
+
+			return true;
+		}
+
+	protected:
+		stream::input_sptr input;   ///< Input .raw file
+		bool first;                 ///< Is this the first time readNextPair() has been called?
+		unsigned long lenData;      ///< Length of song data (where song stops and tags start)
+
+		uint8_t codeShortDelay;     ///< DRO code value used for a short delay
+		uint8_t codeLongDelay;      ///< DRO code value used for a long delay
+		uint8_t codemapLength;      ///< Number of valid entries in codemap array
+		uint8_t codemap[128];       ///< Map DRO code values to OPL registers
+};
+
+
+/// Encode OPL register/value pairs into .dro file data.
+class OPLWriterCallback_DRO_v2: virtual public OPLWriterCallback
+{
+	public:
+		OPLWriterCallback_DRO_v2()
+			throw (stream::error)
+			: usPerTick(DRO_CLOCK),
+			  buffer(new stream::string()),
+			  oplType(0),
+			  codemapLength(0),
+			  cachedDelay(0),
+			  numPairs(0),
+			  msSongLength(0)
+		{
+			memset(this->codemap, 0xff, sizeof(this->codemap));
+		}
+
+		virtual void writeNextPair(const OPLEvent *oplEvent)
+			throw (stream::error)
+		{
+			// Remember the delay for next time in case we get a register we can't write
+			this->cachedDelay += oplEvent->delay;
+
+			assert(oplEvent->chipIndex < 2);
+			if (!(
+				(oplEvent->reg == 0x01) ||
+				(oplEvent->reg == 0x04) ||
+				(oplEvent->reg == 0x05) ||
+				(oplEvent->reg == 0x08) ||
+				(oplEvent->reg == 0xbd) ||
+				((oplEvent->reg >= 0x20) && (oplEvent->reg <= 0x35)) ||
+				((oplEvent->reg >= 0x40) && (oplEvent->reg <= 0x55)) ||
+				((oplEvent->reg >= 0x60) && (oplEvent->reg <= 0x75)) ||
+				((oplEvent->reg >= 0x80) && (oplEvent->reg <= 0x95)) ||
+				((oplEvent->reg >= 0xe0) && (oplEvent->reg <= 0xf5)) ||
+				((oplEvent->reg >= 0xa0) && (oplEvent->reg <= 0xa8)) ||
+				((oplEvent->reg >= 0xb0) && (oplEvent->reg <= 0xb8)) ||
+				((oplEvent->reg >= 0xc0) && (oplEvent->reg <= 0xc8))
+			)) {
+				// We have to skip any invalid registers, otherwise there may not be
+				// enough space in the codemap table to fit the valid registers.
+				std::cerr << "WARNING: Unused OPL register 0x" << std::hex << oplEvent->reg
+					<< " cannot be written to a DROv2 file." << std::endl;
+				return;
+			}
+
+			// Convert ticks into a DRO delay value (which is actually milliseconds)
+			unsigned long delay = this->cachedDelay * this->usPerTick / DRO_CLOCK;
+			this->cachedDelay = 0;
+			this->msSongLength += delay;
+			// Write out the delay in one or more lots of 65535, 255 or less.
+			while (delay > 0) {
+				if (delay > 256) {
+					unsigned long big = (delay >> 8) - 1;
+					if (big > 0xff) big = 0xff;
+					//uint8_t big = (delay >= 0x10000) ? 0xff : (delay >> 8) - 1;
+					// Write out a 'long' delay
+					this->buffer
+						<< u8(DRO2_CMD_LONGDELAY)
+						<< u8(big)
+					;
+					delay -= (big + 1) << 8;
+					this->numPairs++;
+					continue;
+				}
+				assert(delay <= 256);
+				this->buffer
+					<< u8(DRO2_CMD_SHORTDELAY)
+					<< u8(delay - 1) // delay value
+				;
+				this->numPairs++;
+				break; // delay would == 0
+			}
+
+			uint8_t code;
+			if (this->codemap[oplEvent->reg] == 0xff) {
+				code = this->codemapLength++;
+				if (this->codemapLength > 128) {
+					throw stream::error("Cannot write a DROv2 file that uses more than "
+						"127 different OPL registers");
+				}
+				this->codemap[oplEvent->reg] = code;
+			} else {
+				code = this->codemap[oplEvent->reg];
+			}
+
+			if (oplEvent->chipIndex == 1) {
+				code |= 0x80;
+				this->oplType = 1; // OPL3 - could also be dual OPL2 though!
+			}
+			this->buffer
+				<< u8(code)
+				<< u8(oplEvent->val)
+			;
+			this->numPairs++;
+			return;
+		}
+
+		virtual void writeTempoChange(unsigned long usPerTick)
+			throw (stream::error)
+		{
+			assert(usPerTick != 0);
+			this->usPerTick = usPerTick;
+			return;
+		}
+
+		/// Write out all the cached data.
+		void write(stream::output_sptr output)
+			throw (stream::error)
+		{
+			assert(output->tellp() == 12);
+
+			// Write out the header
+			output
+				<< u32le(this->numPairs)     // Song length in pairs
+				<< u32le(this->msSongLength) // Song length in milliseconds
+				<< u8(this->oplType)         // Hardware type (0=OPL2, 1=dual OPL2, 2=OPL3)
+				<< u8(0)                     // Format (0 == interleaved)
+				<< u8(0)                     // Compression (0 == uncompressed)
+				<< u8(DRO2_CMD_SHORTDELAY)   // Short delay code
+				<< u8(DRO2_CMD_LONGDELAY)    // Long delay code
+				<< u8(this->codemapLength)   // Codemap length
+			;
+			for (unsigned int c = 0; c < this->codemapLength; c++) {
+				for (unsigned int i = 0; i < sizeof(this->codemap); i++) {
+					if (this->codemap[i] == c) {
+						output << u8(i);
+					}
+				}
+			}
+			// Write the actual OPL data from the buffer
+			output->write(this->buffer->str().c_str(), this->numPairs << 1);
+
+			return;
+		}
+
+	protected:
+		unsigned long usPerTick;    ///< Latest microseconds per tick value (tempo)
+		stream::string_sptr buffer; ///< Buffer to store output data until finish()
+		uint8_t oplType;            ///< OPL hardware type to write into DRO header
+		uint8_t codemapLength;      ///< Number of valid entries in codemap array
+		uint8_t codemap[256];       ///< Map DRO code values to OPL registers
+		unsigned long cachedDelay;  ///< Delay remembered but not yet written
+		uint32_t numPairs;          ///< Number of data pairs in file
+		uint32_t msSongLength;      ///< Song length in milliseconds
+};
 
 std::string MusicType_DRO_v2::getCode() const
 	throw ()
@@ -52,247 +298,68 @@ MusicType::Certainty MusicType_DRO_v2::isInstance(stream::input_sptr psMusic) co
 	char sig[8];
 	psMusic->seekg(0, stream::start);
 	psMusic->read(sig, 8);
-	if (strncmp(sig, "DBRAWOPL", 8) != 0) return MusicType::DefinitelyNo;
+	if (strncmp(sig, "DBRAWOPL", 8) != 0) return DefinitelyNo;
 
 	// Make sure the header says it's version 2.0
 	// TESTED BY: mus_dro_dosbox_v2_isinstance_c02
 	uint16_t verMajor, verMinor;
 	psMusic >> u16le(verMajor) >> u16le(verMinor);
-	if ((verMajor != 2) || (verMinor != 0)) return MusicType::DefinitelyNo;
+	if ((verMajor != 2) || (verMinor != 0)) return DefinitelyNo;
 
 	// TESTED BY: mus_dro_dosbox_v2_isinstance_c00
-	return MusicType::DefinitelyYes;
+	return DefinitelyYes;
 }
 
-MusicWriterPtr MusicType_DRO_v2::create(stream::output_sptr output, SuppData& suppData) const
+MusicPtr MusicType_DRO_v2::read(stream::input_sptr input, SuppData& suppData) const
 	throw (stream::error)
 {
-	return MusicWriterPtr(new MusicWriter_DRO_v2(output));
+	// Make sure we're at the start, as we'll often be near the end if
+	// isInstance() was just called.
+	input->seekg(0, stream::start);
+
+	OPLReaderCallback_DRO_v2 cb(input);
+	MusicPtr music = oplDecode(&cb, DelayIsPreData, OPL_FNUM_DEFAULT);
+
+	// See if there are any tags present
+	readMalvMetadata(input, music->metadata);
+
+	return music;
 }
 
-MusicReaderPtr MusicType_DRO_v2::open(stream::input_sptr input, SuppData& suppData) const
-	throw (stream::error)
+void MusicType_DRO_v2::write(stream::output_sptr output, SuppData& suppData,
+	MusicPtr music, unsigned int flags) const
+	throw (stream::error, format_limitation)
 {
-	return MusicReaderPtr(new MusicReader_DRO_v2(input));
+	output->write("DBRAWOPL\x02\x00\x00\x00", 12);
+
+	// Call the generic OPL writer.
+	OPLWriterCallback_DRO_v2 cb;
+	oplEncode(&cb, DelayIsPreData, OPL_FNUM_DEFAULT, flags, music);
+	cb.write(output);
+
+	// Write out any metadata
+	writeMalvMetadata(output, music->metadata);
+
+	// Set final filesize to this
+	output->truncate_here();
+
+	return;
 }
 
-SuppFilenames MusicType_DRO_v2::getRequiredSupps(const std::string& filenameMusic) const
+SuppFilenames MusicType_DRO_v2::getRequiredSupps(stream::input_sptr input,
+	const std::string& filenameMusic) const
 	throw ()
 {
 	// No supplemental types/empty list
 	return SuppFilenames();
 }
 
-MusicReader_DRO_v2::MusicReader_DRO_v2(stream::input_sptr input)
-	throw (stream::error) :
-		MusicReader_GenericOPL(DelayIsPreData),
-		input(input)
-{
-	this->input->seekg(12, stream::start);
-	this->input >> u32le(this->lenData);
-	this->input->seekg(6, stream::cur);
-	uint8_t compression, codemapLength;
-	this->input
-		>> u8(compression)
-		>> u8(this->codeShortDelay)
-		>> u8(this->codeLongDelay)
-		>> u8(this->codemapLength)
-	;
-	if (compression != 0) throw stream::error("Compressed DRO files are not implemented (didn't even know they existed)");
-	if (codemapLength > 127) throw stream::error("DRO code map too large");
-	memset(this->codemap, 0xFF, sizeof(this->codemap));
-	this->input->read(this->codemap, this->codemapLength);
-	this->changeSpeed(1000); // 1000us (1ms) per tick
-}
-
-MusicReader_DRO_v2::~MusicReader_DRO_v2()
+Metadata::MetadataTypes MusicType_DRO_v2::getMetadataList() const
 	throw ()
 {
-}
-
-void MusicReader_DRO_v2::rewind()
-	throw ()
-{
-	this->input->seekg(12, stream::start);
-	this->input >> u32le(this->lenData);
-	this->input->seekg(26 + this->codemapLength, stream::start);
-	return;
-}
-
-bool MusicReader_DRO_v2::nextPair(uint32_t *delay, uint8_t *chipIndex, uint8_t *reg, uint8_t *val)
-	throw (stream::error)
-{
-	*delay = 0;
-	*reg = 0; *val = 0; // in case of trailing delay
-	try {
-		uint8_t code, arg;
-nextCode:
-		if (this->lenData == 0) return false; // end of song
-		this->input >> u8(code) >> u8(arg);
-		this->lenData--;
-		if (code == this->codeShortDelay) {
-			*delay += arg + 1;
-			goto nextCode;
-		} else if (code == this->codeLongDelay) {
-			*delay += (arg + 1) << 8;
-			goto nextCode;
-		}
-
-		// High bit indicates which chip to use
-		*chipIndex = code >> 7;
-
-		if ((code & 0x7F) >= this->codemapLength) {
-			std::cerr << "WARNING: DRO file is using codes past the end of the code "
-				"map!" << std::endl;
-			// But continue as we will just use register 0xFF in these cases.
-		}
-		*reg = this->codemap[code & 0x7F];
-		*val = arg;
-	} catch (const stream::incomplete_read&) {
-		return false;
-	}
-	return true; // read some data
-}
-
-// TODO: metadata functions
-
-
-MusicWriter_DRO_v2::MusicWriter_DRO_v2(stream::output_sptr output)
-	throw () :
-		MusicWriter_GenericOPL(DelayIsPreData),
-		output(output),
-		buffer(new stream::string()),
-		dataLen(0),
-		numTicks(0),
-		usPerTick(0),
-		oplType(0),
-		codemapLength(0),
-		cachedDelay(0)
-{
-	memset(this->codemap, 0xff, sizeof(this->codemap));
-}
-
-MusicWriter_DRO_v2::~MusicWriter_DRO_v2()
-	throw ()
-{
-}
-
-void MusicWriter_DRO_v2::start()
-	throw (stream::error)
-{
-	this->output->write("DBRAWOPL\x02\x00\x00\x00", 12);
-
-	return;
-}
-
-void MusicWriter_DRO_v2::finish()
-	throw (stream::error)
-{
-	this->MusicWriter_GenericOPL::finish();
-
-	// Write out some placeholders, which will be overwritten later
-	this->output
-		<< u32le(this->dataLen)    // Song length in pairs
-		<< u32le(this->numTicks)   // Song length in milliseconds
-		<< u8(this->oplType)       // Hardware type (0=OPL2, 1=dual OPL2, 2=OPL3)
-		<< u8(0)                   // Format (0 == interleaved)
-		<< u8(0)                   // Compression (0 == uncompressed)
-		<< u8(0xfe)                // Short delay code
-		<< u8(0xff)                // Long delay code
-		<< u8(this->codemapLength) // Codemap length
-	;
-	for (int c = 0; c < this->codemapLength; c++) {
-		for (int i = 0; i < sizeof(this->codemap); i++) {
-			if (this->codemap[i] == c) {
-				this->output << u8(i);
-			}
-		}
-	}
-	this->output->write(this->buffer->str().c_str(), this->dataLen << 1);
-
-	return;
-}
-
-void MusicWriter_DRO_v2::changeSpeed(uint32_t usPerTick)
-	throw ()
-{
-	this->usPerTick = usPerTick;
-	return;
-}
-
-void MusicWriter_DRO_v2::nextPair(uint32_t delay, uint8_t chipIndex, uint8_t reg, uint8_t val)
-	throw (stream::error)
-{
-	assert(this->usPerTick > 0); // make sure this has been set already
-	assert(chipIndex < 2);
-
-	// Remember the delay for next time in case we get a register we can't write
-	this->cachedDelay += delay;
-
-	if (!(
-		(reg == 0x01) ||
-		(reg == 0x04) ||
-		(reg == 0x05) ||
-		(reg == 0x08) ||
-		(reg == 0xbd) ||
-		((reg >= 0x20) && (reg <= 0x35)) ||
-		((reg >= 0x40) && (reg <= 0x55)) ||
-		((reg >= 0x60) && (reg <= 0x75)) ||
-		((reg >= 0x80) && (reg <= 0x95)) ||
-		((reg >= 0xe0) && (reg <= 0xf5)) ||
-		((reg >= 0xa0) && (reg <= 0xa8)) ||
-		((reg >= 0xb0) && (reg <= 0xb8)) ||
-		((reg >= 0xc0) && (reg <= 0xc8))
-	)) {
-		std::cerr << "WARNING: Unused OPL register 0x" << std::hex << reg
-			<< " cannot be written to a DROv2 file." << std::endl;
-		return;
-	}
-
-	// Convert ticks into a DRO delay value (which is actually milliseconds)
-	delay = this->cachedDelay * this->usPerTick / 1000;
-	this->cachedDelay = 0;
-
-	if (delay > 256) {
-		// Write out a 'long' delay
-		uint16_t big = (delay >> 8) - 1;
-		delay = big & 0xff;
-		this->buffer
-			<< u8(0xff)
-			<< u8(big)
-		;
-		this->dataLen++;
-	}
-	if (delay > 0) {
-		// Write out a 'short' delay
-		this->buffer
-			<< u8(0xfe)
-			<< u8(delay - 1)
-		;
-		this->dataLen++;
-	}
-	uint8_t code;
-	if (this->codemap[reg] == 0xff) {
-		code = this->codemapLength++;
-		if (this->codemapLength > 128) {
-			throw stream::error("Cannot write a DROv2 file that uses more than "
-				"127 different OPL registers");
-		}
-		this->codemap[reg] = code;
-	} else {
-		code = this->codemap[reg];
-	}
-	// TODO: ignore invalid registers (keeping the delay) or there won't be enough codes
-	if (chipIndex == 1) {
-		code |= 0x80;
-		this->oplType = 1; // OPL3 - could also be dual OPL2 though!
-	}
-	this->buffer
-		<< u8(code)
-		<< u8(val)
-	;
-
-	this->dataLen++;
-	this->numTicks += delay;
-	return;
+	Metadata::MetadataTypes types;
+	types.push_back(Metadata::Title);
+	types.push_back(Metadata::Author);
+	types.push_back(Metadata::Description);
+	return types;
 }
--- src/mus-dro-dosbox-v2.hpp
+++ src/mus-dro-dosbox-v2.hpp
@@ -2,7 +2,7 @@
  * @file   mus-dro-dosbox-v2.hpp
  * @brief  Support for the second version of the DOSBox Raw OPL .DRO format.
  *
- * Copyright (C) 2010-2011 Adam Nielsen <malvineous@shikadi.net>
+ * Copyright (C) 2010-2012 Adam Nielsen <malvineous@shikadi.net>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,14 +21,12 @@
 #ifndef _CAMOTO_GAMEMUSIC_MUS_DRO_DOSBOX_V2_HPP_
 #define _CAMOTO_GAMEMUSIC_MUS_DRO_DOSBOX_V2_HPP_
 
-#include <camoto/stream_string.hpp>
 #include <camoto/gamemusic/musictype.hpp>
-#include <camoto/gamemusic/mus-generic-opl.hpp>
 
 namespace camoto {
 namespace gamemusic {
 
-/// MusicType implementation for DRO.
+/// MusicType implementation for DOSBox .dro version 2.
 class MusicType_DRO_v2: virtual public MusicType {
 
 	public:
@@ -42,84 +40,22 @@ class MusicType_DRO_v2: virtual public MusicType {
 		virtual std::vector<std::string> getFileExtensions() const
 			throw ();
 
-		virtual MusicType::Certainty isInstance(stream::input_sptr psMusic) const
+		virtual Certainty isInstance(stream::input_sptr input) const
 			throw (stream::error);
 
-		virtual MusicWriterPtr create(stream::output_sptr output, SuppData& suppData) const
+		virtual MusicPtr read(stream::input_sptr input, SuppData& suppData) const
 			throw (stream::error);
 
-		virtual MusicReaderPtr open(stream::input_sptr input, SuppData& suppData) const
-			throw (stream::error);
+		virtual void write(stream::output_sptr output, SuppData& suppData,
+			MusicPtr music, unsigned int flags) const
+			throw (stream::error, format_limitation);
 
-		virtual SuppFilenames getRequiredSupps(const std::string& filenameMusic) const
+		virtual SuppFilenames getRequiredSupps(stream::input_sptr input,
+			const std::string& filenameMusic) const
 			throw ();
 
-};
-
-/// MusicReader class that understands DRO music files.
-class MusicReader_DRO_v2: virtual public MusicReader_GenericOPL {
-
-	protected:
-		stream::input_sptr input;  ///< Stream of data to read
-		uint32_t lenData;         ///< Length of data to read in reg/val pairs
-		uint8_t codeShortDelay; ///< DRO code value used for a short delay
-		uint8_t codeLongDelay;  ///< DRO code value used for a long delay
-		uint8_t codemapLength;  ///< Number of valid entries in codemap array
-		uint8_t codemap[128];   ///< Map DRO code values to OPL registers
-
-	public:
-
-		MusicReader_DRO_v2(stream::input_sptr input)
-			throw (stream::error);
-
-		virtual ~MusicReader_DRO_v2()
+		virtual Metadata::MetadataTypes getMetadataList() const
 			throw ();
-
-		virtual void rewind()
-			throw ();
-
-		virtual bool nextPair(uint32_t *delay, uint8_t *chipIndex, uint8_t *reg, uint8_t *val)
-			throw (stream::error);
-
-		// TODO: metadata functions
-};
-
-/// MusicWriter class that can produce DRO music files.
-class MusicWriter_DRO_v2: virtual public MusicWriter_GenericOPL {
-
-	protected:
-		stream::output_sptr output;      ///< Stream to write data into
-		stream::string_sptr buffer; ///< Buffer to store output data until finish()
-		int dataLen;              ///< Length of data in buffer, in pairs
-		uint32_t numTicks;        ///< Total number of ticks in the song
-		int usPerTick;            ///< Latest microseconds per tick value (tempo)
-		uint8_t oplType;          ///< OPL hardware type to write into DRO header
-		uint8_t codemapLength;    ///< Number of valid entries in codemap array
-		uint8_t codemap[256];     ///< Map DRO code values to OPL registers
-		uint16_t cachedDelay;     ///< Delay remembered but not yet written
-
-	public:
-
-		MusicWriter_DRO_v2(stream::output_sptr output)
-			throw ();
-
-		virtual ~MusicWriter_DRO_v2()
-			throw ();
-
-		virtual void start()
-			throw (stream::error);
-
-		virtual void finish()
-			throw (stream::error);
-
-		virtual void changeSpeed(uint32_t usPerTick)
-			throw ();
-
-		virtual void nextPair(uint32_t delay, uint8_t chipIndex, uint8_t reg, uint8_t val)
-			throw (stream::error);
-
-		// TODO: metadata functions
-
 };
 
 } // namespace gamemusic
