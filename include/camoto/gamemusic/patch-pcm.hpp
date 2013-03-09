67a57b9df58d8315a719709a63234477ac07ef84
Adam Nielsen <malvineous@shikadi.net>
2013-03-09 20:30:31 +1000

    Add support for PCM (sampled) instruments

---
+++ include/camoto/gamemusic/patch-pcm.hpp
@@ -0,0 +1,56 @@
+/**
+ * @file   patch-pcm.hpp
+ * @brief  Derived Patch for PCM patches.
+ *
+ * Copyright (C) 2010-2013 Adam Nielsen <malvineous@shikadi.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _CAMOTO_GAMEMUSIC_PATCH_PCM_HPP_
+#define _CAMOTO_GAMEMUSIC_PATCH_PCM_HPP_
+
+#include <boost/shared_array.hpp>
+#include <camoto/gamemusic/patch.hpp>
+#include <stdint.h>
+
+namespace camoto {
+namespace gamemusic {
+
+/// Descendent of Patch for storing PCM instruments.
+struct PCMPatch: public Patch
+{
+	/// Default constructor sets everything to zero/defaults.
+	PCMPatch();
+
+	uint32_t sampleRate; ///< Sampling rate in Hertz to get a middle-C note
+	uint8_t bitDepth;    ///< Sample size in bits (8/16)
+	uint8_t numChannels; ///< Channel count (1=mono, 2=stereo)
+
+	uint32_t loopStart;  ///< Beginning of loop (offset of first sample)
+	uint32_t loopEnd;    ///< End of loop, 0=no loop (offset of last sample+1)
+
+	uint32_t lenData;    ///< Size of data in bytes
+
+	/// Actual sample data
+	boost::shared_array<uint8_t> data;
+};
+
+/// Shared pointer to a Patch.
+typedef boost::shared_ptr<PCMPatch> PCMPatchPtr;
+
+} // namespace gamemusic
+} // namespace camoto
+
+#endif // _CAMOTO_GAMEMUSIC_PATCH_PCM_HPP_
---
+++ src/patch-pcm.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file   patch-pcm.cpp
+ * @brief  Implementation of PCM Patch class, for managing sampled instruments.
+ *
+ * Copyright (C) 2010-2013 Adam Nielsen <malvineous@shikadi.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <camoto/gamemusic/patch-pcm.hpp>
+
+using namespace camoto::gamemusic;
+
+PCMPatch::PCMPatch()
+	:	sampleRate(8363),
+		bitDepth(8),
+		numChannels(1)
+{
+}
