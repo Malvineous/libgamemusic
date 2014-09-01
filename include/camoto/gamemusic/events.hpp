e9f196cc3d6e49d74cc1ce6ca6b77f97efaaa55a
Adam Nielsen <malvineous@shikadi.net>
2014-09-01 23:02:48 +1000

    Move TrackInfo declaration from events.hpp to music.hpp

--- include/camoto/gamemusic/events.hpp
+++ include/camoto/gamemusic/events.hpp
@@ -42,62 +42,6 @@ const unsigned int MAX_CHANNELS = 256;
 /// Type to use for all microsecond-per-tick values.
 typedef double tempo_t;
 
-/// Information about a track, shared across all patterns.
-/**
- * This controls which channel a track's events are played on.
- */
-struct DLL_EXPORT TrackInfo
-{
-	/// Channel type
-	enum ChannelType {
-		UnusedChannel,
-		AnyChannel,
-		OPLChannel,
-		OPLPercChannel,
-		MIDIChannel,
-		PCMChannel,
-	};
-
-	/// What type of channel this track will be played through.
-	ChannelType channelType;
-
-	/// Channel index.
-	/**
-	 * When type is:
-	 *
-	 * - AnyChannel: this value is 0-255.  The value
-	 *   is not so important, but only one note can be played on each channel
-	 *   at a time, across all tracks.  It is not valid to write any tracks
-	 *   with this channel type set.  All AnyChannel tracks must be mapped to
-	 *   other values before a song is passed to a format writer.  This is to
-	 *   prevent every format writer from having to map channels itself.
-	 *   @todo: Make a utility function to do this.
-	 *
-	 * - OPLChannel: this value is 0 to 8 for normal OPL channels on
-	 *   chip 1, and 9 to 17 for chip 2.  Some events are global and will affect
-	 *   the whole chip regardless of what track they are played on.
-	 *
-	 * - OPLPercChannel: this value is 4 for bass drum, 3 for snare, 2 for tomtom,
-	 *   1 for top cymbal, 0 for hi-hat.  Other values are invalid.
-	 *
-	 * - MIDIChannel: this value is 0 to 15, with 9 being percussion.
-	 *
-	 * - PCMChannel: this value is the channel index starting at 0.  For some
-	 *   formats like .mod, this affects the panning of the channel.
-	 *
-	 * Note that OPL percussion mode uses channels 6, 7 and 8, so it is not
-	 * valid for a song to have OPLChannel events on these channels while
-	 * OPLPercChannel events are present.  This may happen temporarily during a
-	 * format conversion, but this state must be resolved by the time the
-	 * data is written out to a file.
-	 */
-	unsigned int channelIndex;
-};
-
-/// Vector of trackinfo (a channel map)
-typedef std::vector<TrackInfo> TrackInfoVector;
-
-
 /// Base class to represent events in the file.
 /**
  * Will be extended by descendent classes to hold format-specific data.
